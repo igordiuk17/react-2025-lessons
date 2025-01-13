649593b9a15ddbe37f6ee51f8e364ba1e18f772b
igordiuk17 <igordiuk17@gmail.com>
2025-01-13 22:01:50 +0200

    Commit7 hw3

---
+++ src/pages/UserPage.tsx
@@ -0,0 +1,7 @@
+export const UserPage = ()=>{
+return(
+    <>
+        UserPage
+    </>
+);
+};
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,10 @@
 import {createBrowserRouter} from "react-router";
 import MainLayout from "../layouts/MainLayout.tsx";
-
+// import {UserPage} from "../pages/UserPage.tsx";
 
 export const routes = createBrowserRouter([
     {
-        path:"/",element:<MainLayout/>
+        path: "/", element: <MainLayout/>
+
     }
 ])

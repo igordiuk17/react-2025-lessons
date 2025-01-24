98dee5c0ec6c5e83fe9996645cd704f95480f8c3
igordiuk17 <igordiuk17@gmail.com>
2025-01-24 23:40:06 +0200

    Commit 11

--- src/servises/GeneralApiServises.ts
+++ src/servises/GeneralApiServises.ts
@@ -1,7 +1,7 @@
-// import {IPost} from "../models/APosts.ts";
-// import {IUsers} from "../models/AUsers.ts";
+// import {IPost} from "../models/IPosts.ts";
+// import {IUsers} from "../models/IUsers.ts";
 import {IbaseResponsModel} from "../models/IbaseResponsModel.ts";
-import {IPost} from "../models/APosts.ts";
+import {IPost} from "../models/IPosts.ts";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 export const getAll= async <T,>(endpoint:string):Promise<T> =>{
